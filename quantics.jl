450188099702d39031dfef479f5d85b354e29c17
victor <victor.vanthilt@gmail.com>
2026-01-13 15:58:14 +0100

    change implementation to get rid of tensormaps

--- quantics.jl
+++ quantics.jl
@@ -1,12 +1,12 @@
 # small interface for time ordered integrals using quantics
 using TensorKit
 
-struct QuanticTT{E, S}
-    data::Vector{TensorKit.TensorMap{E, S, 2, 1}}
+struct QuanticTT{E}
+    data::Vector{Array{E, 3}}
     N::Int
     function QuanticTT(data)
         N = length(data)
-        return new{eltype(data[1]), spacetype(data[1])}(data, N)
+        return new{eltype(data[1])}(data, N)
     end
 end
 
@@ -25,11 +25,16 @@ end
     Generate an quantics TT representation of (a + ) sin(ω t) over [0, 1[ on 2^N evenly spaced gridpoints.
 """
 function sin_TT(ω::Float64, N::Int)
-    vl = TensorMap([1 / (2im), -1 / (2im)], ℂ^1 ← ℂ^2)
-    vr = ones(ComplexF64, ℂ^2 ← ℂ^1)
+    # Left boundary: [1/(2im), -1/(2im)]
+    vl = ones(ComplexF64, 1, 2)
+    vl[1, 1] = 1 / (2im)
+    vl[1, 2] = -1 / (2im)
+
+    # Right boundary: [1, 1]ᵀ
+    vr = ones(ComplexF64, 2, 1)
 
     tensors = map(1:N) do α # loop over sites
-        A = zeros(ComplexF64, ℂ^2 ⊗ ℂ^2 ← ℂ^2)
+        A = zeros(ComplexF64, 2, 2, 2) # left, physical, right
         for nα in 1:2 # loop over physical index
             A[1, nα, 1] = exp(1im * ω * 2.0^(α - 1 - N) * (nα - 1))
             A[2, nα, 2] = exp(- 1im * ω * 2.0^(α - 1 - N) * (nα - 1))
@@ -37,16 +42,23 @@ function sin_TT(ω::Float64, N::Int)
         return A
     end
 
-    @tensor tensors[1][-1 -2; -3] := vl[-1; 1] * tensors[1][1 -2; -3]
-    @tensor tensors[end][-1 -2; -3] := tensors[end][-1 -2; 1] * vr[1; -3]
+    # Absorb boundaries
+    @tensor tensors[1][-1 -2 -3] := vl[-1 1] * tensors[1][1 -2 -3]
+    @tensor tensors[end][-1 -2 -3] := tensors[end][-1 -2 1] * vr[1 -3]
     return QuanticTT(tensors)
 end
 function sin_TT(a::Float64, ω::Float64, N::Int)
-    vl = TensorMap([1 / (2im), -1 / (2im), a], ℂ^1 ← ℂ^3)
-    vr = ones(ComplexF64, ℂ^3 ← ℂ^1)
+    # Left boundary: [1/(2im), -1/(2im), a]
+    vl = ones(ComplexF64, 1, 3)
+    vl[1, 1] = 1 / (2im)
+    vl[1, 2] = -1 / (2im)
+    vl[1, 3] = a
+
+    # Right boundary: [1, 1, 1]ᵀ
+    vr = ones(ComplexF64, 3, 1)
 
     tensors = map(1:N) do α # loop over sites
-        A = zeros(ComplexF64, ℂ^3 ⊗ ℂ^2 ← ℂ^3)
+        A = zeros(ComplexF64, 3, 2, 3) # left, physical, right
         for nα in 1:2 # loop over physical index
             A[1, nα, 1] = exp(1im * ω * 2.0^(α - 1 - N) * (nα - 1))
             A[2, nα, 2] = exp(- 1im * ω * 2.0^(α - 1 - N) * (nα - 1))
@@ -55,6 +67,7 @@ function sin_TT(a::Float64, ω::Float64, N::Int)
         return A
     end
 
+    # Absorb boundaries
     @tensor tensors[1][-1 -2; -3] := vl[-1; 1] * tensors[1][1 -2; -3]
     @tensor tensors[end][-1 -2; -3] := tensors[end][-1 -2; 1] * vr[1; -3]
     return QuanticTT(tensors)
@@ -74,20 +87,12 @@ function (qt::QuanticTT)(x::Float64)
     xstring = [parse(Int, c) for c in xstring] .+ 1 # [1, 2, 2, 1, ...]
     xstring = reverse(xstring) # little endian
 
-    # Tensors to put on the physical legs
-    n0tensor = zeros(scalartype(qt.data[1]), one(ComplexSpace) ← ℂ^2)
-    n0tensor[1] = one(scalartype(n0tensor))
-
-    n1tensor = zeros(scalartype(qt.data[1]), one(ComplexSpace) ← ℂ^2)
-    n1tensor[2] = one(scalartype(n1tensor))
-
-    ntensor = [n0tensor, n1tensor]
-    @tensor answer[-1; -2] := qt[1][-1 1; -2] * ntensor[xstring[1]][1] # evaluate left boundary
-    for (pos, char) in enumerate(xstring[2:end])
-        @tensor answer[-1; -2] := answer[-1; 1] * qt[pos + 1][1 2; -2] * ntensor[char][2]
+    a = ones(1)
+    for (pos, char) in enumerate(xstring)
+        cur = qt[pos][:, char, :] # impose physical index
+        @tensor a[-1] := a[1] * cur[1, -1]
     end
-
-    return only(answer.data)
+    return a
 end
 
 function check_accuracy(N, t)
@@ -98,7 +103,7 @@ function check_accuracy(N, t)
 end
 
 ω = 0.1
-QT = sin_TT(ω, 40)
+QT = sin_TT(1.1, ω, 40)
 actualsin(x) = 2 + sin(ω * x)
 
 @time sin_TT(0.0, 0.1, 40)(0.946)
